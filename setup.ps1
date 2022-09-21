c4890e86e08558c32fdbc76695bac9f83fc4d5ab
mon-jai <91261297+mon-jai@users.noreply.github.com>
2022-09-21 14:42:04 +0800

    .

--- setup.ps1
+++ setup.ps1
@@ -1,5 +1,7 @@
 # https://stackoverflow.com/a/43905715
-# Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://github.dev/mon-jai/network-programming/blob/main/setup.ps1'))
+# Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://github.com/mon-jai/network-programming/blob/main/setup.ps1'))
+
+Write-Output "Downloading Python..."
 
 # https://stackoverflow.com/a/73534796
 if (
@@ -9,10 +11,14 @@ if (
 
 # https://stackoverflow.com/a/43477248
 $ProgressPreference = 'SilentlyContinue'
-
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 Invoke-WebRequest -Uri $Matches.url -OutFile "$env:USERPROFILE/python.exe"
+
+Write-Output "Installing Python..."
+
 & "$env:USERPROFILE/python.exe" /quiet InstallAllUsers=0 PrependPath=1 Include_test=0
+
+Write-Output "Setting up VSCode..."
 
 Invoke-WebRequest -Uri "https://github.dev/mon-jai/network-programming/blob/main/settings.json" -OutFile "C:\Users\LAB1223\AppData\Roaming\Code\User\settings.json"
 code --install-extension ms-python.python
