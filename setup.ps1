ec62be0b904e61710002b0a68817fe881c83bf81
mon-jai <91261297+mon-jai@users.noreply.github.com>
2022-09-21 14:34:32 +0800

    .

--- setup.ps1
+++ setup.ps1
@@ -1,5 +1,5 @@
 # https://stackoverflow.com/a/43905715
-# Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://gist.githubusercontent.com/mon-jai/f3a21942f2e2334ec12faefc5851c1f3/raw/a9b67d8d95db9906183cecadacd11ccb0f068a2f/setup.ps1'))
+# Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://github.dev/mon-jai/network-programming/blob/main/setup.ps1'))
 
 # https://stackoverflow.com/a/73534796
 if (
@@ -14,6 +14,6 @@ $ProgressPreference = 'SilentlyContinue'
 Invoke-WebRequest -Uri $Matches.url -OutFile "$env:USERPROFILE/python.exe"
 & "$env:USERPROFILE/python.exe" /quiet InstallAllUsers=0 PrependPath=1 Include_test=0
 
-Invoke-WebRequest -Uri "https://gist.githubusercontent.com/mon-jai/f3a21942f2e2334ec12faefc5851c1f3/raw/c94255432e3097f2f2eac3662211186e788b86dd/settings.json" -OutFile "C:\Users\LAB1223\AppData\Roaming\Code\User\settings.json"
+Invoke-WebRequest -Uri "https://github.dev/mon-jai/network-programming/blob/main/settings.json" -OutFile "C:\Users\LAB1223\AppData\Roaming\Code\User\settings.json"
 code --install-extension ms-python.python
 code --install-extension formulahendry.code-runner
