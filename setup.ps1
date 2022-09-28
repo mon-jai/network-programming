c182788141ed2d1c47ad1cb5d7227db653224a6d
mon-jai <91261297+mon-jai@users.noreply.github.com>
2022-09-28 10:34:29 +0800

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -18,14 +18,10 @@ Write-Output "Downloading Python..."
 
 # https://stackoverflow.com/a/73534796
 if (
-  (Invoke-RestMethod 'https://www.python.org/downloads/') -notmatch 
+  ((New-Object System.Net.WebClient).DownloadString('https://www.python.org/downloads/')) -notmatch
   '\bhref="(?<url>.+?\.exe)"\s*>\s*Download Python (?<version>\d+\.\d+\.\d+)'
 ) { throw "Could not determine latest Python version and download URL" }
 
-# https://stackoverflow.com/a/43477248
-$ProgressPreference = 'SilentlyContinue'
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-Invoke-WebRequest -Uri $Matches.url -OutFile "$env:USERPROFILE/python.exe"
 
 Write-Output "Installing Python..."
 
@@ -33,7 +29,7 @@ Write-Output "Installing Python..."
 
 Write-Output "Setting up VSCode..."
 
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/mon-jai/network-programming/main/settings.json" -OutFile "C:\Users\LAB1223\AppData\Roaming\Code\User\settings.json"
+(New-Object System.Net.WebClient).DownloadFile("https://raw.githubusercontent.com/mon-jai/network-programming/main/settings.json", "$env:AppData\Code\User\settings.json")
 code --install-extension ms-python.python
 code --install-extension formulahendry.code-runner
 code --install-extension github.github-vscode-theme
