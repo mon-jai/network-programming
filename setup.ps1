56addc4183ce1b1480828d4cd17d3baddf74e0a4
mon-jai <91261297+mon-jai@users.noreply.github.com>
2022-09-28 13:43:26 +0800

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -1,7 +1,7 @@
 # https://stackoverflow.com/a/43905715
 # Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/mon-jai/network-programming/main/setup.ps1'))
 
-Write-Output "Setting language...";
+Write-Output "Setting language..."
 
 # https://stackoverflow.com/a/51374938
 Set-Culture en-US
@@ -14,36 +14,29 @@ $languageList[1].InputMethodTips.Clear()
 $languageList[1].InputMethodTips.Add('0404:{531FDEBF-9B4C-4A43-A2AA-960E8FCDC732}{4BDF9F03-C7D3-11D4-B2AB-0080C882687E}')
 Set-WinUserLanguageList $languageList -Force
 
-Start-Job {
-  $pythonDownloadPath = "$env:USERPROFILE/python.exe"
+Write-Output "Downloading Python..."
 
-  Write-Output "Installing Python..."
+# https://stackoverflow.com/a/73534796
+if (
+  (Invoke-RestMethod 'https://www.python.org/downloads/') -notmatch 
+  '\bhref="(?<url>.+?\.exe)"\s*>\s*Download Python (?<version>\d+\.\d+\.\d+)'
+) { throw "Could not determine latest Python version and download URL" }
 
-  # https://stackoverflow.com/a/73534796
-  if (
-    ((New-Object System.Net.WebClient).DownloadString('https://www.python.org/downloads/')) -notmatch
-    '\bhref="(?<url>.+?\.exe)"\s*>\s*Download Python (?<version>\d+\.\d+\.\d+)'
-  ) { throw "Could not determine latest Python version and download URL" }
-  (New-Object System.Net.WebClient).DownloadFile($Matches.url, $pythonDownloadPath)
+# https://stackoverflow.com/a/43477248
+$ProgressPreference = 'SilentlyContinue'
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+Import-Module BitsTransfer
+Start-BitsTransfer $Matches.url "$env:USERPROFILE/python.exe"
 
-  # https://stackoverflow.com/a/1742758
-  Start-Process "$pythonDownloadPath" -ArgumentList "/quiet", "InstallAllUsers=0", "PrependPath=1", "Include_test=0" -NoNewWindow -Wait
-  Remove-Item $pythonDownloadPath
+Write-Output "Installing Python..."
 
-  Write-Output "Installing Python... Done"
-}
+& "$env:USERPROFILE/python.exe" /quiet InstallAllUsers=0 PrependPath=1 Include_test=0
 
-Start-Job {
-  Write-Output "Setting up VSCode..."
+Write-Output "Setting up VSCode..."
 
-  (New-Object System.Net.WebClient).DownloadFile("https://raw.githubusercontent.com/mon-jai/network-programming/main/settings.json", "$env:AppData\Code\User\settings.json")
-  code --install-extension ms-python.python --force
-  code --install-extension formulahendry.code-runner --force
-  code --install-extension github.github-vscode-theme --force
-
-  Write-Output "Setting up VSCode... Done"
-}
-
-Get-Job | Receive-Job -Wait -AutoRemoveJob
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/mon-jai/network-programming/main/settings.json" -OutFile "C:\Users\LAB1223\AppData\Roaming\Code\User\settings.json"
+code --install-extension ms-python.python
+code --install-extension formulahendry.code-runner
+code --install-extension github.github-vscode-theme
 
 Write-Output "Done!"
