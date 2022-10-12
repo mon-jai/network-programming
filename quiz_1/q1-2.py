ea0ab13f19b265a8acceca8fd163447f31b8908f
mon-jai <91261297+mon-jai@users.noreply.github.com>
2022-10-12 13:33:40 +0800

    Add files via upload

---
+++ quiz_1/q1-2.py
@@ -0,0 +1,47 @@
+from typing import List
+from urllib.request import urlretrieve
+from zipfile import ZipFile
+import csv
+
+
+def extract_and_print(row):
+    print(f'{row[0]} {row[1]} {row[12]}')
+
+
+zipFileName, _ = urlretrieve('https://data.ntpc.gov.tw/api/datasets/71CD1490-A2DF-4198-BEF1-318479775E8A/csv/zip'
+                             )
+
+N = int(input())
+
+with ZipFile(zipFileName) as zipFile:
+    for fileName in zipFile.namelist():
+        csvFileName = zipFile.extract(fileName, './')
+
+        with open(csvFileName, 'r', encoding='UTF-8') as csvFile:
+            plots = csv.reader(csvFile)
+
+            '''
+            0 "sno"
+            1 sna
+            2 tot
+            3 sbi
+            4 sarea
+            5 mday
+            6 lat
+            7 lng
+            8 ar
+            9 sareaen
+            10 snaen
+            11 aren
+            12 bemp
+            13 act
+            print('\n'.join(f'{index} {col}' for index, col in enumerate(plots.__next__())))
+            '''
+
+            extract_and_print(plots.__next__())
+
+            data = [row for row in plots if int(row[12]) > N]
+            data.sort(key=lambda row: int(row[12]), reverse=True)
+
+            for row in sorted(data, key=lambda row: row[0]):
+                extract_and_print(row)
