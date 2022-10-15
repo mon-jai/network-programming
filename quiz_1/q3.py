a3a59fbd72807427c8ab65b2192bf79cb55504ed
mon-jai <91261297+mon-jai@users.noreply.github.com>
2022-10-16 00:33:34 +0800

    Update q3.py

--- quiz_1/q3.py
+++ quiz_1/q3.py
@@ -1,37 +1,61 @@
-# 抓取7-eleven各門市資訊
-import requests  # 抓取網頁的套件
-import pandas as pd  # 分析資料的套件
-# 建立一個縣市的list
-city = ['基隆市', '台北市', '新北市', '桃園市']
-# 使用迴圈依序取得每一個城市的門市資訊
-roadAddressRecord = {}
-for index, city in enumerate(city):
-    # 剛在網頁開發者模式觀察到的Post發出的資訊
-    data = {'strTargetField': 'COUNTY', 'strKeyWords': '%s' % city}
-    # res 取得網頁所有資料
-    res = requests.post(
-        'https://www.ibon.com.tw/retail_inquiry_ajax.aspx', data=data)
-    # 本次城市門市個數
-    count = pd.read_html(res.text, header=0)[0].shape[0]
-    # 取得本次城市門市資料
-    data = pd.read_html(res.text, header=0)[0]
+from urllib.request import urlretrieve
+from xml.etree.ElementTree import SubElement, ElementTree, Element
 
-    for i in range(count):
-        # 第三欄資料為地址 iloc[i,2]
-        fullAddress = data.iloc[i, 2]
-        # 找城市名稱開頭,路結尾的地址字串
-        start = fullAddress.find(city[0])
-        end = fullAddress.find('路')+1
-        #end = fullAddress.find('街')+1
-        if end < 3:
-            continue  # 空的資料跳過
-        roadAddress = fullAddress[start:end]
-        if roadAddress not in roadAddressRecord:
-            roadAddressRecord[roadAddress] = 1
+xmlFileName = "data1.xml"
+outputFileName = "data2.xml"
+
+urlretrieve(
+    'https://data.ntpc.gov.tw/api/datasets/71CD1490-A2DF-4198-BEF1-318479775E8A/xml/preview',
+    filename=xmlFileName
+)
+
+tree = ElementTree(file=xmlFileName)
+root = tree.getroot()
+
+row = SubElement(root, "row")
+
+sno = SubElement(row, "sno")
+sna = SubElement(row, "sna")
+tot = SubElement(row, "tot")
+sbi = SubElement(row, "sbi")
+sarea = SubElement(row, "sarea")
+
+sno.text = "1033"
+sna.text = "家樂福新店店"
+tot.text = "30"
+sbi.text = "29"
+sarea.text = "新店區"
+
+for shop in root:
+    shop_to_modify = False
+
+    for shop_data in shop:
+        if shop_data.tag == "sno" and shop_data.text == "1018":
+            shop_to_modify = True
         else:
-            roadAddressRecord[roadAddress] += 1
+            break
 
-print('\n'.join(
-    f'{key}, {value}'
-    for key, value in sorted(roadAddressRecord.items(), key=lambda item: item[1], reverse=True)[:3]
-))
+    if shop_to_modify == True:
+        for shop_data in shop:
+            if shop_data.tag == "sbi":
+                shop_data.text = '0'
+
+tree.write(outputFileName, encoding="utf-8")
+
+tree_2 = ElementTree(file=outputFileName)
+root_2 = tree.getroot()
+
+print('sno sna tot sbi')
+
+for shop in root_2:
+    shop_to_print = False
+
+    for shop_data in shop:
+        if shop_data.tag == "sarea" and shop_data.text == "新店區":
+            shop_to_print = True
+
+    if shop_to_print == True:
+        for shop_data in shop:
+            if shop_data.tag in ["sno", "sna", "tot", "sbi"]:
+                print(shop_data.text, end=' ')
+        print('')
