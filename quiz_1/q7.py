75f257710411a105c1d84b8753a5507bb5974451
mon-jai <91261297+mon-jai@users.noreply.github.com>
2022-10-16 21:24:03 +0800

    Update q7.py

--- quiz_1/q7.py
+++ quiz_1/q7.py
@@ -11,6 +11,9 @@ class EarthQuake (NamedTuple):
     intensity: float
     location: str
 
+    def __str__(self):
+        return f'{self.time} {self.depth:g} {self.intensity} {self.location}'
+
 
 def compare(a: EarthQuake, b: EarthQuake):
     if a.intensity > b.intensity:
@@ -27,7 +30,7 @@ bsObj = BeautifulSoup(html, "lxml")
 table = bsObj.find("table", class_="Btable worldTable")
 assert isinstance(table, Tag)
 
-earthQuakes: list[EarthQuake] = [
+earthQuakes = [
     EarthQuake(
         tds[0].text, float(tds[3].text),
         float(tds[4].text), tds[5].text.strip()
@@ -40,9 +43,4 @@ earthQuakes: list[EarthQuake] = [
 earthQuakes.sort(key=cmp_to_key(compare), reverse=True)
 
 print('地震時間 深度 規模 地震位置')
-print('\n'.join(
-    [
-        f'{earthQuake.time} {earthQuake.depth:g} {earthQuake.intensity} {earthQuake.location}'
-        for earthQuake in earthQuakes
-    ][:3]
-))
+print('\n'.join([str(earthQuake) for earthQuake in earthQuakes][:3]))
