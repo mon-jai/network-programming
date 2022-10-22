de9de949e7445de49d3a9a79e2f33996cfb7d5f6
mon-jai <91261297+mon-jai@users.noreply.github.com>
2022-10-22 22:22:07 +0800

    Update q8.py

--- quiz_1/q8.py
+++ quiz_1/q8.py
@@ -5,20 +5,34 @@ import requests
 from bs4 import BeautifulSoup
 from bs4.element import Tag
 
-def compare(a, b):
-    city_1 = re.search(r'^.*[市縣]', a[2]).group(0)
-    city_2 = re.search(r'^.*[市縣]', b[2]).group(0)
-    door_1 = int(re.search(r'(\d+)號', a[2]).group(1))
-    door_2 = int(re.search(r'(\d+)號', b[2]).group(1))
 
-    if city_1 > city_2:
-        return 1 
-    elif city_1 < city_2:
+def extract_city(address: str) -> str:
+    result = re.search(r'(?P<city>^.*[市縣])', address)
+    assert isinstance(result, re.Match)
+    return result.group('city')
+
+
+def extract_house_number(address: str):
+    result = re.search(r'(?P<house_number>\d+)號', address)
+    assert isinstance(result, re.Match)
+    return int(result.group('house_number'))
+
+
+def compare(a: tuple[str, str, str], b: tuple[str, str, str]):
+    city_a = extract_city(a[2])
+    city_b = extract_city(b[2])
+    house_number_a = extract_house_number(a[2])
+    house_number_b = extract_house_number(b[2])
+
+    if city_a > city_b:
+        return 1
+    elif city_a < city_b:
         return -1
     else:
-        return 1 if door_1 > door_2 else -1
+        return 1 if house_number_a > house_number_b else -1
 
-def printRow(row):
+
+def printRow(row: tuple[str, str, str]):
     print(' '.join(column for column in row))
 
 
@@ -26,19 +40,16 @@ strKeyWords = input()
 
 html = requests.post(
     'https://www.ibon.com.tw/retail_inquiry_ajax.aspx',
-    data={"strTargetField": "MIXFIELD", "strKeyWords": strKeyWords}
+    data={"strTargetField": "MIXFIELD", "strKeyWords": strKeyWords},
 ).text
 
 bsObj = BeautifulSoup(html, "lxml")
 table = bsObj.find("table")
 assert isinstance(table, Tag)
 
-data = [
+data: list[tuple[str, str, str]] = [
     (tds[0].text, tds[1].text, tds[2].text)
-    for tds in (
-        tr.findAll("td")
-        for tr in table.findAll("tr")
-    )
+    for tds in (tr.findAll("td") for tr in table.findAll("tr"))
 ]
 
 printRow(data[0])
@@ -48,6 +59,5 @@ data.sort(key=cmp_to_key(compare))
 
 for row in data:
     printRow(row)
-
 
 # 承德路
