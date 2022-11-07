873cece791877364bdf226a3f9abf3a5aba50c42
mon-jai <91261297+mon-jai@users.noreply.github.com>
2022-11-07 23:18:53 +0800

    Update setup-environment.ps1

--- setup-environment.ps1
+++ setup-environment.ps1
@@ -3,49 +3,53 @@
 
 $pythonDownloadPath = "$Env:USERPROFILE/python.exe"
 
-Write-Output "Setting language..."
+Start-Job -Name 'Setup language' -ErrorAction Stop -ScriptBlock {
+  # https://stackoverflow.com/a/51374938
+  Set-Culture en-US
+  Set-WinSystemLocale -SystemLocale en-US
+  Set-WinUILanguageOverride -Language en-US
 
-# https://stackoverflow.com/a/51374938
-Set-Culture en-US
-Set-WinSystemLocale -SystemLocale en-US
-Set-WinUILanguageOverride -Language en-US
+  $languageList = New-WinUserLanguageList en-US
+  $languageList.Add('zh-Hant-TW')
+  $languageList[1].InputMethodTips.Clear()
+  $languageList[1].InputMethodTips.Add('0404:{531FDEBF-9B4C-4A43-A2AA-960E8FCDC732}{4BDF9F03-C7D3-11D4-B2AB-0080C882687E}')
+  Set-WinUserLanguageList $languageList -Force
+  Write-Information "Setup language completed"
+}
 
-$languageList = New-WinUserLanguageList en-US
-$languageList.Add('zh-Hant-TW')
-$languageList[1].InputMethodTips.Clear()
-$languageList[1].InputMethodTips.Add('0404:{531FDEBF-9B4C-4A43-A2AA-960E8FCDC732}{4BDF9F03-C7D3-11D4-B2AB-0080C882687E}')
-Set-WinUserLanguageList $languageList -Force
+Start-Job -Name 'Install Python' -ErrorAction Stop -ScriptBlock {
+  # https://stackoverflow.com/a/73534796
+  if (
+    (Invoke-RestMethod 'https://www.python.org/downloads/') -notmatch 
+    '\bhref="(?<url>.+?\.exe)"\s*>\s*Download Python (?<version>\d+\.\d+\.\d+)'
+  ) { throw "Could not determine latest Python version and download URL" }
 
-Write-Output "Downloading Python..."
+  # https://stackoverflow.com/a/21423159
+  Import-Module BitsTransfer
+  Start-BitsTransfer $Matches.url $pythonDownloadPath
 
-# https://stackoverflow.com/a/73534796
-if (
-  (Invoke-RestMethod 'https://www.python.org/downloads/') -notmatch 
-  '\bhref="(?<url>.+?\.exe)"\s*>\s*Download Python (?<version>\d+\.\d+\.\d+)'
-) { throw "Could not determine latest Python version and download URL" }
+  Start-Process "$pythonDownloadPath" -ArgumentList "/quiet", "PrependPath=1" -NoNewWindow -Wait
+  Remove-Item $pythonDownloadPath
 
-# https://stackoverflow.com/a/21423159
-Import-Module BitsTransfer
-Start-BitsTransfer $Matches.url $pythonDownloadPath
+  # https://stackoverflow.com/a/67796873
+  pip config set global.trusted-host "pypi.org files.pythonhosted.org pypi.python.org"
+  pip install -U autopep8
 
-Write-Output "Installing Python..."
+  Write-Information "Install Python completed"
+}
 
-Start-Process "$pythonDownloadPath" -ArgumentList "/quiet", "PrependPath=1" -NoNewWindow -Wait
-Remove-Item $pythonDownloadPath
+Start-Job -Name 'Setup VSCode' -ErrorAction Stop -ScriptBlock {
+  # https://stackoverflow.com/a/36705460
+  # https://stackoverflow.com/a/36751445
+  Remove-Item "$Env:USERPROFILE/.vscode/extensions" -Force -Recurse -ErrorAction SilentlyContinue
 
-# https://stackoverflow.com/a/67796873
-pip config set global.trusted-host "pypi.org files.pythonhosted.org pypi.python.org"
-pip install -U autopep8
+  Invoke-WebRequest -Uri "https://raw.githubusercontent.com/mon-jai/network-programming/main/.vscode/settings.json" -OutFile "$Env:APPDATA\Code\User\settings.json"
+  code --install-extension ms-python.python --force
+  code --install-extension formulahendry.code-runner --force
+  code --install-extension github.github-vscode-theme --force
 
-Write-Output "Setting up VSCode..."
+  Write-Information "Setup VSCode completed"
+}
 
-# https://stackoverflow.com/a/36705460
-# https://stackoverflow.com/a/36751445
-Remove-Item "$Env:USERPROFILE/.vscode/extensions" -Force -Recurse -ErrorAction SilentlyContinue
-
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/mon-jai/network-programming/main/.vscode/settings.json" -OutFile "$Env:APPDATA\Code\User\settings.json"
-code --install-extension ms-python.python --force
-code --install-extension formulahendry.code-runner --force
-code --install-extension github.github-vscode-theme --force
-
-Write-Output "Done!"
+Get-Job | Receive-Job -Wait -ErrorAction Stop
+Write-Information "Done!"
