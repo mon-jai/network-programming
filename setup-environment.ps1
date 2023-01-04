6888cc0f6fc007991dd26a0faa1969e33d6c2075
mon-jai <91261297+mon-jai@users.noreply.github.com>
2023-01-04 20:50:45 +0800

    Update setup-environment.ps1

--- setup-environment.ps1
+++ setup-environment.ps1
@@ -3,7 +3,7 @@ Param([switch]$InstallPython)
 # Copyright 2022 Loh Ka Hong | Licensed under MIT
 
 # Throw an statement-terminating error when "the setting is overridden by a policy defined at a more specific scope", https://stackoverflow.com/a/60549569
-# https://stackoverflow.com/a/18782669
+# Redirect all streams to $null, https://stackoverflow.com/a/6461021
 # https://stackoverflow.com/a/68777742
 # . { Set-ExecutionPolicy Bypass -Scope Process -Force } *> $null; & ([scriptblock]::Create((irm 'https://raw.githubusercontent.com/mon-jai/network-programming/main/setup-environment.ps1'))) -InstallPython
 
